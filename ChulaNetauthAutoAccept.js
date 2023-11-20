dddfa62c03c0140d2c5b6957d5d6801e4a422dca
JustinJira <jus.jira@gmail.com>
2023-11-20 19:03:10 +0700

    feat: Added file

---
+++ ChulaNetauthAutoAccept.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Chula Netauth Auto Accept
+// @namespace    http://tampermonkey.net/
+// @version      0.2
+// @description  Auto accept agreement's checkbox
+// @author       JusJira
+// @match        https://netauth1.it.chula.ac.th/login
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=ac.th
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    let checkbox = document.getElementsByName("accept")
+    checkbox[0].checked = true;
+})();
