b064cc48727947fde7aa1d75f01a98dbbd7c39b1
JustinJira <jus.jira@gmail.com>
2023-12-03 16:52:43 +0700

    refactor: Update homepage link

--- ChulaNetauthAutoAccept.js
+++ ChulaNetauthAutoAccept.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         Chula Netauth Auto Accept
-// @namespace    http://tampermonkey.net/
-// @license MIT
-// @version      0.2
+// @namespace    https://greasyfork.org/en/scripts/480362
+// @version      0.3
 // @description  Auto accept agreement's checkbox
 // @author       JusJira
 // @match        https://netauth1.it.chula.ac.th/login
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=ac.th
+// @grant        none
 // ==/UserScript==
- 
+
 (function() {
     'use strict';
     let checkbox = document.getElementsByName("accept")
